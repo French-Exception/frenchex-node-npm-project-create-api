34f07ad4600063b4a5d1c9d0e9df3f4abfc75265
Stephane ERARD <serard06@protonmail.com>
2019-12-17 14:38:13 +0100

    Working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function run() {
+const lib = require("@frenchex/npm-project-create-lib");
+async function which(bin) {
+    return await lib.which(bin);
 }
-exports.run = run;
+exports.which = which;
+function npmProjectCreate(logger) {
+    const npmProjectCreateCommand = new lib.NpmProjectCreateCommand(logger);
+    return npmProjectCreateCommand;
+}
+exports.npmProjectCreate = npmProjectCreate;
+function gitClone(logger) {
+    const gitCloneCommand = new lib.GitCloneCommand(logger);
+    return gitCloneCommand;
+}
+exports.gitClone = gitClone;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export function run(){
-    
+import * as lib from "@frenchex/npm-project-create-lib";
+
+export async function which(bin: string): Promise<string> {
+    return await lib.which(bin);
+}
+
+export function npmProjectCreate(logger: lib.Logger) {
+
+    const npmProjectCreateCommand = new lib.NpmProjectCreateCommand(logger);
+
+    return npmProjectCreateCommand;
+}
+
+export function gitClone(logger: lib.Logger) {
+    const gitCloneCommand = new lib.GitCloneCommand(logger);
+
+    return gitCloneCommand;
 }
